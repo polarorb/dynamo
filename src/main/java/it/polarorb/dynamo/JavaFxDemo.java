a5557403fe9ffa49a0bb681f5982d33e0f15df9d
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2016-10-05 20:00:00 -0400

    Sample JavaFx Application Added

---
+++ src/main/java/it/polarorb/dynamo/JavaFxDemo.java
@@ -0,0 +1,18 @@
+package it.polarorb.dynamo;/**
+ * Created by erikrahtjen on 10/5/16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFxDemo extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
