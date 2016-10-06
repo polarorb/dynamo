2bcd55f0bf6fd516e78feed09ca366789972ac1f
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2016-10-05 23:33:21 -0400

    LibGDX Converseion

---
+++ desktop/src/it/polarorb/dynamo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.polarorb.dynamo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.polarorb.dynamo.DynamoDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DynamoDemo(), config);
+	}
+}
