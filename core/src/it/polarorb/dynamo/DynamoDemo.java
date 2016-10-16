1ff4917383dfe62be342331e75342a58020b94dd
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2016-10-15 21:09:09 -0400

    The first isometric tile was rendered in this iteration. Completely programmatic. Definitely going to be some refactoring later. But good start!

--- core/src/it/polarorb/dynamo/DynamoDemo.java
+++ core/src/it/polarorb/dynamo/DynamoDemo.java
@@ -4,15 +4,20 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DynamoDemo extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private PolygonSpriteBatch spriteBatch;
+	private IsometricBox box;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		spriteBatch = new PolygonSpriteBatch();
+		box = new IsometricBox();
 		img = new Texture("core/assets/badlogic.jpg");
 	}
 
@@ -23,11 +28,16 @@ public class DynamoDemo extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		spriteBatch.begin();
+		spriteBatch.draw(box.getFloorPolygonRegion(), 0, 0);
+		spriteBatch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		spriteBatch.dispose();
 	}
 }
--- core/src/it/polarorb/dynamo/IsometricBox.java
+++ core/src/it/polarorb/dynamo/IsometricBox.java
@@ -1,6 +1,13 @@
 package it.polarorb.dynamo;
 
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
+import com.badlogic.gdx.graphics.g2d.PolygonSprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Polygon;
+
+import java.util.List;
 
 /**
  * Created by erikrahtjen on 10/15/16.
@@ -15,6 +22,9 @@ public class IsometricBox {
     private final float depth;
 
     private Location location;
+    private List<Polygon> polygonList;
+    private float blX;
+    private float blY;
 
     public IsometricBox() {
         this(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_DEPTH);
@@ -26,7 +36,37 @@ public class IsometricBox {
         this.depth = depth;
     }
 
-    public void render(ShapeRenderer shapeRenderer, Location location) {
+    public float[] getFloorVertices() {
+        return new float[]{
+                blX,blY+height/2, // BL
+                blX+width/2, blY, // BR
+                blX+width, blY + height/2, //TR
+                blX + width/2, blY + height //TL
+        };
+    }
 
+    public PolygonRegion getFloorPolygonRegion() {
+        // Creating the color filling (but textures would work the same way)
+        Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pix.setColor(0xDEADBEFF); // DE is red, AD is green and BE is blue.
+        pix.fill();
+
+        Texture textureSolid = new Texture(pix);
+
+        PolygonRegion polyReg = new PolygonRegion(new TextureRegion(textureSolid),
+                getFloorVertices(), new short[] {
+                0, 1, 2,         // Two triangles using vertex indices.
+                2, 3, 0          // Take care of the counter-clockwise direction.
+        });
+
+        return polyReg;
+    }
+
+    public PolygonSprite getFloorPolygonSprite() {
+        PolygonSprite poly;
+        PolygonRegion polyReg = getFloorPolygonRegion();
+        poly = new PolygonSprite(polyReg);
+        poly.setOrigin(blX, blY);
+        return poly;
     }
 }
