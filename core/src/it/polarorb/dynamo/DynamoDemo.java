31cade30eef7d2c7865216c24187b17338ef3532
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2016-10-17 23:54:42 -0400

    Made the background color yellow

--- core/src/it/polarorb/dynamo/DynamoDemo.java
+++ core/src/it/polarorb/dynamo/DynamoDemo.java
@@ -82,7 +82,7 @@ public class DynamoDemo extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.begin();
